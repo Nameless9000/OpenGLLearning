9071eb87f68932cf0f485e41766665ec72db8121
Nameless 9000 <dev@name1ess.ga>
2024-05-08 19:19:09 +0100

    Block demo

--- BlockDemo/main.cpp
+++ BlockDemo/main.cpp
@@ -1,90 +1,84 @@
 #include <GL/glut.h>
-#include <iostream>
+#include <GLES/gl.h>
 
-using namespace std;
+float angle = 0.0;
 
-int rx = 100, ry = 125;
-int xCenter = 250, yCenter = 250;
-
-void myinit(void)
+static void init()
 {
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
+}
+
+static void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    double aspect = (double)viewport[2] / (double)viewport[3];
+    gluPerspective(25, aspect, 1, 100);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // move back a bit
+    glTranslatef(0, 0, -35);
+
+    static float angle = 0;
+    angle += 1.0f;
+
+    glRotatef(angle, 0.1, 0.2, 0.5);
+
+    // draw blocks
+    glPushMatrix();
+    glTranslatef(0, 0, 0);
+    glColor3ub(255, 255, 255);
+    glutSolidCube(1);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0, 1, 0);
+    glColor3ub(255, 0, 255);
+    glutSolidCube(1);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0, 2, 1);
+    glColor3ub(255, 0, 0);
+    glutSolidCube(1);
+    glPopMatrix();
+
+    glutSwapBuffers();
 }
 
-void setPixel(GLint x, GLint y)
+static void reshape(int w, int h)
 {
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
+    glViewport(0, 0, w, h);
 }
-void ellipseMidPoint()
-{
-    float x = 0;
-    float y = ry;
-    float p1 = ry * ry - (rx * rx) * ry + (rx * rx) * (0.25);
-    float dx = 2 * (ry * ry) * x;
-    float dy = 2 * (rx * rx) * y;
-    glColor3ub(rand() % 255, rand() % 255, rand() % 255);
-    while (dx < dy) {
-        setPixel(xCenter + x, yCenter + y);
-        setPixel(xCenter - x, yCenter + y);
-        setPixel(xCenter + x, yCenter - y);
-        setPixel(xCenter - x, yCenter - y);
-        if (p1 < 0) {
-            x = x + 1;
-            dx = 2 * (ry * ry) * x;
-            p1 = p1 + dx + (ry * ry);
-        } else {
-            x = x + 1;
-            y = y - 1;
-            dx = 2 * (ry * ry) * x;
-            dy = 2 * (rx * rx) * y;
-            p1 = p1 + dx - dy + (ry * ry);
-        }
-    }
-    glFlush();
 
-    float p2 = (ry * ry) * (x + 0.5) * (x + 0.5) + (rx * rx) * (y - 1) * (y - 1) - (rx * rx) * (ry * ry);
-    glColor3ub(rand() % 255, rand() % 255, rand() % 255);
-    while (y > 0) {
-        setPixel(xCenter + x, yCenter + y);
-        setPixel(xCenter - x, yCenter + y);
-        setPixel(xCenter + x, yCenter - y);
-        setPixel(xCenter - x, yCenter - y);
-        if (p2 > 0) {
-            x = x;
-            y = y - 1;
-            dy = 2 * (rx * rx) * y;
-            p2 = p2 - dy + (rx * rx);
-        } else {
-            x = x + 1;
-            y = y - 1;
-            dy = dy - 2 * (rx * rx);
-            dx = dx + 2 * (ry * ry);
-            p2 = p2 + dx - dy + (rx * rx);
-        }
-    }
-    glFlush();
-}
-void display()
+static void timer(int extra)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 0.0, 0.0);
-    glPointSize(2.0);
-    ellipseMidPoint();
-    glFlush();
+    glutPostRedisplay();
+    glutTimerFunc(16, timer, 0);
 }
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitWindowSize(640, 480);
-    glutInitWindowPosition(10, 10);
-    glutCreateWindow("User_Name");
-    myinit();
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+    glutCreateWindow("BlockDemo");
+
     glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutTimerFunc(0, timer, 0);
+
+    init();
+
     glutMainLoop();
     return 0;
 }
